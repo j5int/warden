df5968a628d4d78ecd67d27ed4fb4eec578aa240
benm <benm@dori.(none)>
2012-12-05 11:11:46 +0200

    Renamed thread stuff, added config option to sentry warden

--- warden_sentry.py
+++ warden_sentry.py
@@ -3,42 +3,42 @@ import sys
 import time
 import ctypes
 import threading
-from logan import importer
-from logan.runner import run_app
-from django.core import management
 import sentry.utils.runner as runner
-from ThreadRaise import thread_async_raise, get_thread_id
-from sentry.services import http, udp
+from warden_thread_mon import thread_async_raise, get_thread_id
 
 class SentryManager:
+    """
+    Doc stuff
+    """
 
-    def __init__(self):
-        old_argv = sys.argv
-        sys.argv = ['sentry','--config=~/.sentry/sentry.conf.py','start']
-        print(sys.argv)
+    def __init__(self, config_path):
+        self.old_argv = []
+        self.config_path = config_path
         self.st = None
 
     def start_sentry(self):
+        # store old argument array
+        self.old_argv = sys.argv
+
+        # set new argument array
+        sys.argv = ['sentry', '--config=%s' % self.config_path, 'start']
+
+        # setup and start thread
+        print('Starting Sentry with %s' % str(sys.argv))
         self.st = self.SentryThread()
         self.st.start()
 
     def stop_sentry(self):
         self.st.fire_keyboard_int()
+        self.st.join()
 
     class SentryThread(threading.Thread):
 
         def __init__(self):
             threading.Thread.__init__(self)
 
-
-
         def run(self):
-
-            #svc = http.SentryHTTPServer(False, None, None, None)
-            #svc.run()
-
             runner.main()
-
 
         def fire_keyboard_int(self):
             thread_async_raise(self, KeyboardInterrupt)
@@ -50,7 +50,7 @@ def countdown(delay):
         print((delay * ' ') + str(delay))
 
 if __name__=='__main__':
-    sm = SentryManager()
+    sm = SentryManager('~/.sentry/sentry.conf.py')
     sm.start_sentry()
 
     countdown(100)
