b447702d58c961e3f8c20b846898119e59545a10
benm <benmeier42@gmail.com>
2013-01-23 12:23:46 +0200

    Removed windows platform thing

--- warden/settings.py
+++ warden/settings.py
@@ -1,4 +1,3 @@
-import platform
 import logging
 
 # WARDEN GENERAL
@@ -12,11 +11,13 @@ STDOUT_LEVEL = logging.DEBUG
 DIAMOND_CONFIG = '~/.diamond/etc/diamond/diamond.conf'
 # logging level of diamond. this produces a lot of logs if set to info/debug
 DIAMOND_STDOUT_LEVEL = logging.ERROR
+# path to diamond_root
+DIAMOND_ROOT = None
 
 # GENTRY
 # ----------------
 # path to the gentry settings.py file
-GENTRY_SETTINGS_PATH = '/home/benm/work/venvs/gentrytest/lib/python2.7/site-packages/gentry-0.0.1-py2.7.egg/gentry/settings.py'
+GENTRY_SETTINGS_PATH = None
 # optional path to an overriding sentry secret key
 SENTRY_KEY_FILE = None
 
@@ -28,9 +29,4 @@ GRAPHITE_ROOT = '~/.graphite'
 # the path to the carbon config is derived from the GRAPHITE_ROOT unless defined here
 CARBON_CONFIG = '~/.graphite/conf/carbon.conf'
 
-# windows overrides
 
-if platform.system() == 'Windows':
-    GRAPHITE_ROOT = 'C:\\Graphite'
-    CARBON_CONFIG = 'C:\\Graphite\\conf\\carbon.conf'
-    DIAMOND_CONFIG = 'C:\\.diamond\\diamond.conf'
